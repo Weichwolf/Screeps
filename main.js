c4b6e7a6ce64e49d3faa469036553f3d82623d52
Weichwolf <github@outshine.de>
2020-04-16 08:43:47 +0200

    test

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const MainController = require('controller.main');
 
 global.TASK_TRANSFER = 1;
 
+
 module.exports.loop = function () {
     let mainController = new MainController();
     mainController.run();
