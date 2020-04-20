8f90df721ca24c4913ebf10c9683dab3ca6ab04d
Weichwolf <56591353+Weichwolf@users.noreply.github.com>
2020-04-20 10:34:31 +0200

    usedCapacityLastTick

--- creep.worker.js
+++ creep.worker.js
@@ -234,14 +234,19 @@ class WorkerCreep extends BaseCreep {
             this.upgrade(creep);
 	    } else {
 	        var target = Game.getObjectById(creep.memory.targetIdUpgrade);
-            var energy = creep.store.getUsedCapacity(RESOURCE_ENERGY);
             
 	        if(creep.upgradeController(target) == ERR_NOT_IN_RANGE) {
 	            this.moveToTarget(creep, target);
 	        } else if(creep.upgradeController(target) == ERR_NOT_ENOUGH_RESOURCES) {
                 creep.memory.targetIdUpgrade = null;
                 this.changeTask(creep, 'recycle');
-            }            
+            } else if(creep.upgradeController(target) == OK) {
+                let energy = creep.store.getUsedCapacity(RESOURCE_ENERGY);
+                if(creep.memory.usedCapacityLastTick) {
+                    creep.memory.upgradeEnergy += creep.memory.usedCapacityLastTick - energy;
+                }
+                creep.memory.usedCapacityLastTick = energy; 
+            }
 	    }
     }                
 }
--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const mainController = require('controller.main');
 
+global.MOVING_AVERAGE_GENERATIONS = 10;
+
 module.exports.loop = function () {
     mainController.run();
     mainController.cleanup();
